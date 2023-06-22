51363741aed21517efa72f77b80d358bad0f2eaa
devansjo <dan@thebookofeveryone.com>
2023-06-22 14:48:33 +0200

    feat: add configuration options

--- app/snowflake-renderer.js
+++ app/snowflake-renderer.js
@@ -1,32 +1,33 @@
 import $ from 'jquery';
-import _ from 'lodash';
 
-$.fn.rotate = function(degrees) {
-    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
+$.fn.rotate = function (degrees) {
+    $(this).css({
+        '-webkit-transform': 'rotate(' + degrees + 'deg)',
+        '-moz-transform': 'rotate(' + degrees + 'deg)',
+        '-ms-transform': 'rotate(' + degrees + 'deg)',
+        'transform': 'rotate(' + degrees + 'deg)'
+    });
     return $(this);
 };
 
-const defaultOptions = {
-    name: 'Your Name',
-    arms: 6,
-    max_font: 200,
-    min_font: 6,
-    arm_length: 256,
-};
 
 export default function buildSnowflake($target, options) {
 
-    _.defaults(options, defaultOptions);
-
     $('.arm').remove();
 
-    const name_text = options.name.length > 0 ? options.name : defaultOptions.name;
+    $('#snowflake_holder').css({
+        'font-family': options.font_family,
+        'font-weight': options.font_weight
+
+    });
+    $('#example').css({
+        'font-family': options.font_family,
+        'font-weight': options.font_weight
+    });
+    const name_text = options.name.length > 0 ? options.name : "your name";
     const chars = [];
 
-    for(let t = 0; t < name_text.length; t++) {
+    for (let t = 0; t < name_text.length; t++) {
         const char = name_text.substr(t, 1);
 
         // step between the max and min font sizes
@@ -43,7 +44,7 @@ export default function buildSnowflake($target, options) {
     }
 
     const arm = chars.map((char) => {
-        return `<div class="char" style="font-size: ${char.font_size}px">${char.char}</div>`;
+        return `<div class="char" style=" font-size: ${char.font_size}px;">${char.char}</div>`;
     }).join('');
 
     const primary_arm = `
@@ -57,22 +58,21 @@ export default function buildSnowflake($target, options) {
     $('#snowflake').append(primary_arm);
 
     // already made arm 0, just clone it into the remaining arms
-    for(var i = 1; i < options.arms; i++) {
+    for (var i = 1; i < options.arms; i++) {
         var new_arm = $('#arm_0').clone();
-        new_arm.attr('id',' arm_' + i).rotate((360/options.arms) * i);
+        new_arm.attr('id', ' arm_' + i).rotate((360 / options.arms) * i);
         $target.append(new_arm);
         $('#snowflake').append(new_arm);
     }
 
-    $('.poster_framed_1_snowflake').html($target.clone().attr("class", "snowflake_static_1").attr("id", "").css({ 'color' : '#' + options.text_colour }) );
+    $('.poster_framed_1_snowflake').html($target.clone().attr("class", "snowflake_static_1").attr("id", "").css({ 'color': '#' + options.text_colour }));
     $('.poster_framed_1_text').html("the snowflake of " + options.name);
-    $('.mockup, .card_front').css( { 'background-color' : '#' + options.background_colour });
-    $('.snowflake_static_1 div').css( { 'color' : '#' + options.text_colour });
-    $('.poster_framed_1_text').css( { 'color' : '#' + options.text_colour });
+    $('.mockup, .card_front').css({ 'background-color': '#' + options.background_colour });
+    $('.snowflake_static_1 div').css({ 'color': '#' + options.text_colour });
+    $('.poster_framed_1_text').css({ 'color': '#' + options.text_colour });
 
     // Copy styles and content into the static snowflake builder page
-    $('#snowflake_background').css({ 'background-color' : '#' + options.background_colour });
-    $('#snowflake_background #snowflake_maker #snowflake_holder div').css({ 'color' : '#' + options.text_colour });
-    $('#name_text').html("the snowflake of " + options.name).css({ 'color' : '#' + options.text_colour });
+    $('#snowflake_maker #snowflake_holder div').css({ 'color': '#' + options.text_colour });
+    $('#name_text').html("the snowflake of " + options.name).css({ 'color': '#' + options.text_colour });
 
 }
